638e82fb6926f99f4d2da402fa3c9b8babfa8789
dyzezZ <genok222@gmail.com>
2023-10-02 23:01:22 +0300

    Commit

---
+++ VR_project/VR/Assets/project.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class project : MonoBehaviour
+{
+    public GameObject cylinder;
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+    public void start_cylinder()
+    {
+        cylinder.SetActive(!cylinder.activeSelf);
+    }
+    public void start_cube()
+    {
+     cube.SetActive(!cube.activeSelf);
+    }
+}
